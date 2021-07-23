3eadab72c26368e507cd6dba5adeea6c6a1fb1f7
Motoki Murata <ksnk.r0118.bb@gmail.com>
2021-07-23 20:04:19 +0900

    table

--- assets/database.py
+++ assets/database.py
@@ -2,6 +2,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from assets.models import Data
 
 import datetime
 import os
@@ -20,7 +21,7 @@ Base = declarative_base()
 Base.query = db_session.query_property()
 
 def init_db():    
-    Base.metadata.create_all(bind=engine)
+    Base.metadata.create_all(bind=engine, tables=[Data.__table__])
 
 def read_data():
     from assets import models   
