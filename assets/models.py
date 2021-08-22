a2a6cd4c3de4f7fff6c7bf06fa7729bcaac83baa
Motoki Murata <ksnk.r0118.bb@gmail.com>
2021-08-22 23:32:20 +0900

    unique

--- assets/models.py
+++ assets/models.py
@@ -4,20 +4,23 @@ from assets.database import Base
 from datetime import datetime as dt
 from datetime import timedelta as td
 from datetime import timezone as tz
+from sqlalchemy.schema import UniqueConstraint
 
 #Table情報
 class Data(Base):
     #TableNameの設定
     __tablename__ = "data"
+    #Unique制約
+    __table_args__ = (UniqueConstraint('Vessel','Carrier','Voyage','Service','Pod','ETA','Berthing'),{})
     #Column情報を設定する
     id = Column(Integer, primary_key=True)
-    Vessel = Column(String, unique=False)
-    Carrier = Column(String, unique=False)
-    Voyage = Column(String, unique=False)
-    Service = Column(String, unique=False)
-    Pod = Column(String, unique=False)
-    ETA = Column(String, unique=False)
-    Berthing = Column(String, unique=False)
+    Vessel = Column(String)
+    Carrier = Column(String)
+    Voyage = Column(String)
+    Service = Column(String)
+    Pod = Column(String)
+    ETA = Column(String)
+    Berthing = Column(String)
     timestamp = Column(DateTime, default=dt.now())
 
     def __init__(self, Vessel=None, Carrier=None, Voyage=None,Service=None, Pod=None, ETA=None, Berthing=None ,timestamp=None):
